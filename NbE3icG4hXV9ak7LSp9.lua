f979ed4a7fc2dc50fa46f871b90e5f194eedc7ac
x6wzeslost <109306431+x6wzeslost@users.noreply.github.com>
2023-05-15 21:33:32 +0700

    Add files via upload

---
+++ NbE3icG4hXV9ak7LSp9.lua
@@ -0,0 +1,245 @@
+if game.PlaceId == 6296321810 then
+    repeat task.wait() until game:IsLoaded()
+    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+    local Window = OrionLib:MakeWindow({Name = "Aquiuxa", HidePremium = false, SaveConfig = false, ConfigFolder = "7ztjmTpjvU"})
+	local Tab = Window:MakeTab({
+	Name = "Tab 1",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+    Tab:AddButton({
+	Name = "Button!",
+	Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(923.735901, -9.58500195, 160.27861, -1.1920929e-07, 0, -1.00000012, 0, 1, 0, 1.00000012, 0, -1.1920929e-07)
+        wait(1.8)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(3476.17456, 34.5769463, -1524.03064, -1, 0, 0, 0, 1, 0, 0, 0, -1)
+  	end    
+})
+    Tab:AddButton({
+	Name = "Full Brightness",
+	Callback = function()
+        game:GetService("Lighting").Brightness = 2
+        game:GetService("Lighting").ClockTime = 14
+        game:GetService("Lighting").FogEnd = 100000
+        game:GetService("Lighting").GlobalShadow = false
+        game:GetService("Lighting").OutdoorAmbient = Color3.fromRGB(128, 128, 128)
+  	end    
+})
+    OrionLib:Init()
+end
+
+if game.PlaceId == 6301638949 then
+    repeat task.wait() until game:IsLoaded()
+    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+    local Window = OrionLib:MakeWindow({Name = "Aquiuxa", HidePremium = false, SaveConfig = false, ConfigFolder = "7ztjmTpjvU"})
+	local Tab = Window:MakeTab({
+	Name = "Tab 1",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+    Tab:AddButton({
+	Name = "Button!",
+	Callback = function()
+  		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1272.8075, 201.615051, -2537.38843, 0, 0, 1, 0, 1, -0, -1, 0, 0)
+  		wait(5)
+  		local TweenService = game:GetService("TweenService")
+local Tw = TweenService:Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(1, Enum.EasingStyle.Linear, Enum.EasingDirection.Out,0,false,0), 
+{CFrame = CFrame.new(2140.85132, 257.123474, -2967.0293, 0.999790728, -2.2898532e-09, -0.0204560775, 2.41584108e-09, 1, 6.13423046e-09, 0.0204560775, -6.18236573e-09, 0.999790728)}):Play()
+workspace.Interactive.SelectionButton.Buttons.Next.Button.Selection.HoldDuration = 0
+end    
+})
+    Tab:AddButton({
+	Name = "Full Brightness",
+	Callback = function()
+        game:GetService("Lighting").Brightness = 2
+        game:GetService("Lighting").ClockTime = 14
+        game:GetService("Lighting").FogEnd = 100000
+        game:GetService("Lighting").GlobalShadow = false
+        game:GetService("Lighting").OutdoorAmbient = Color3.fromRGB(128, 128, 128)
+  	end    
+})
+    OrionLib:Init()
+end
+    if game.PlaceId == 6373539583 then
+    repeat task.wait() until game:IsLoaded()
+    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+    local Window = OrionLib:MakeWindow({Name = "Aquiuxa", HidePremium = false, SaveConfig = false, ConfigFolder = "7ztjmTpjvU"})
+	local Tab = Window:MakeTab({
+	Name = "Tab 1",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+    Tab:AddButton({
+	Name = "Button!",
+	Callback = function()
+	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.LockedDoor:GetModelCFrame()
+end    
+})
+
+    Tab:AddButton({
+	Name = "Full Brightness",
+	Callback = function()
+        game:GetService("Lighting").Brightness = 2
+        game:GetService("Lighting").ClockTime = 14
+        game:GetService("Lighting").FogEnd = 100000
+        game:GetService("Lighting").GlobalShadow = false
+        game:GetService("Lighting").OutdoorAmbient = Color3.fromRGB(128, 128, 128)
+  	end    
+})
+OrionLib:Init()
+end
+if game.PlaceId == 6406571212 then
+    repeat task.wait() until game:IsLoaded()
+    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+    local Window = OrionLib:MakeWindow({Name = "Aquiuxa", HidePremium = false, SaveConfig = false, ConfigFolder = "7ztjmTpjvU"})
+	local Tab = Window:MakeTab({
+	Name = "Tab 1",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+    Tab:AddButton({
+	Name = "Button!",
+	Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(234.037552, 100.099548, -597.244629, 0, 0, 1, 0, 1, -0, -1, 0, 0)
+end    
+})
+
+    Tab:AddButton({
+	Name = "Full Brightness",
+	Callback = function()
+        game:GetService("Lighting").Brightness = 2
+        game:GetService("Lighting").ClockTime = 14
+        game:GetService("Lighting").FogEnd = 100000
+        game:GetService("Lighting").GlobalShadow = false
+        game:GetService("Lighting").OutdoorAmbient = Color3.fromRGB(128, 128, 128)
+  	end    
+})
+OrionLib:Init()
+end
+if game.PlaceId == 6425178683 then
+    repeat task.wait() until game:IsLoaded()
+    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+    local Window = OrionLib:MakeWindow({Name = "Aquiuxa", HidePremium = false, SaveConfig = false, ConfigFolder = "7ztjmTpjvU"})
+	local Tab = Window:MakeTab({
+	Name = "Tab 1",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+    Tab:AddButton({
+	Name = "Button!",
+	Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.LockedDoor:GetModelCFrame()
+        wait(.1)
+        local TweenService = game:GetService("TweenService")
+local Tw = TweenService:Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(5, Enum.EasingStyle.Linear, Enum.EasingDirection.Out,0,false,0), 
+{CFrame = CFrame.new(1027.89478, 73.3553696, -353.885986, -0.179361314, 1.00804563e-07, -0.983783245, 4.33182237e-08, 1, 9.45685414e-08, 0.983783245, -2.56538062e-08, -0.179361314)}):Play()
+wait(6.2)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1115.18591, 84.9654999, -359.600281, -0.0773830265, -1.13200301e-07, -0.99700141, -5.48315482e-09, 1, -1.13115178e-07, 0.99700141, -3.28648198e-09, -0.0773830265)
+workspace.Interactive.SelectionButton.Buttons.Next.Button.Selection.HoldDuration = 0
+end    
+})
+
+    Tab:AddButton({
+	Name = "Full Brightness",
+	Callback = function()
+        game:GetService("Lighting").Brightness = 2
+        game:GetService("Lighting").ClockTime = 14
+        game:GetService("Lighting").FogEnd = 100000
+        game:GetService("Lighting").GlobalShadow = false
+        game:GetService("Lighting").OutdoorAmbient = Color3.fromRGB(128, 128, 128)
+  	end    
+})
+OrionLib:Init()
+end
+if game.PlaceId == 6472459099 then
+    repeat task.wait() until game:IsLoaded()
+    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+    local Window = OrionLib:MakeWindow({Name = "Aquiuxa", HidePremium = false, SaveConfig = false, ConfigFolder = "7ztjmTpjvU"})
+	local Tab = Window:MakeTab({
+	Name = "Tab 1",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+    Tab:AddButton({
+	Name = "Button!",
+	Callback = function()
+	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(2410.20679, -14.1816654, 2307.24097, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+
+end    
+})
+
+    Tab:AddButton({
+	Name = "Full Brightness",
+	Callback = function()
+	pcall(function()
+        game:GetService("Lighting").Brightness = 2
+        game:GetService("Lighting").ClockTime = 14
+        game:GetService("Lighting").FogEnd = 100000
+        game:GetService("Lighting").GlobalShadow = false
+        game:GetService("Lighting").OutdoorAmbient = Color3.fromRGB(128, 128, 128)
+  	end)
+  	end    
+})
+OrionLib:Init()
+end
+if game.PlaceId == 6682163754 then
+    repeat task.wait() until game:IsLoaded()
+    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+    local Window = OrionLib:MakeWindow({Name = "Aquiuxa", HidePremium = false, SaveConfig = false, ConfigFolder = "7ztjmTpjvU"})
+	local Tab = Window:MakeTab({
+	Name = "Tab 1",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+    Tab:AddButton({
+	Name = "Button!",
+	Callback = function()
+	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(242.771713, 34.3152275, 449.323639, 0, 0, 1, 1, 0, 0, 0, 1, 0)
+
+end    
+})
+
+    Tab:AddButton({
+	Name = "Full Brightness",
+	Callback = function()
+	pcall(function()
+        game:GetService("Lighting").Brightness = 2
+        game:GetService("Lighting").ClockTime = 14
+        game:GetService("Lighting").FogEnd = 100000
+        game:GetService("Lighting").GlobalShadow = false
+        game:GetService("Lighting").OutdoorAmbient = Color3.fromRGB(128, 128, 128)
+  	end)
+  	end    
+})
+OrionLib:Init()
+end
+if game.PlaceId == 6682164423 then
+    repeat task.wait() until game:IsLoaded()
+    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+    local Window = OrionLib:MakeWindow({Name = "Aquiuxa", HidePremium = false, SaveConfig = false, ConfigFolder = "7ztjmTpjvU"})
+	local Tab = Window:MakeTab({
+	Name = "Tab 1",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+    Tab:AddButton({
+	Name = "Button!",
+	Callback = function()
+	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-630.295837, 947.804138, -1365.46277, 0.984571934, -4.52400535e-08, 0.174980238, 3.87041688e-08, 1, 4.07646823e-08, -0.174980238, -3.33632961e-08, 0.984571934)
+end    
+})
+
+    Tab:AddButton({
+	Name = "Full Brightness",
+	Callback = function()
+	pcall(function()
+        game:GetService("Lighting").Brightness = 2
+        game:GetService("Lighting").ClockTime = 14
+        game:GetService("Lighting").FogEnd = 100000
+        game:GetService("Lighting").GlobalShadow = false
+        game:GetService("Lighting").OutdoorAmbient = Color3.fromRGB(128, 128, 128)
+  	end)
+  	end    
+})
+OrionLib:Init()
+end
